dd270214caa1a21867ea3c36ae240702850f26f6
elint <kumarsamip03sc@gmail.com>
2024-03-01 20:40:02 +0545

    setup swagger ui

---
+++ src/main/java/com/app/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.app.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+	
+	@Bean
+	public OpenAPI springShopOpenAPI() {
+		return new OpenAPI().info(new Info().title("Spring boot Application")
+			.description("Backend APIs for config vault app")
+			.version("v1.0.0")
+			.license(new License().name("License").url("/")))
+			.externalDocs(new ExternalDocumentation().description("Config Vault Documentation")
+			.url("http://localhost:8080/swagger-ui/index.html"));
+	}
+	
+}
